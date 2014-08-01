c496257da4daf0de7e91d200c01cb5490138151c
Richard Lehane <richard.lehane@gmail.com>
2014-08-01 15:21:54 +1000

    update

--- types/filetime_test.go
+++ types/filetime_test.go
@@ -10,6 +10,6 @@ func TestTime(t *testing.T) {
 	ft := FileTime{0xEC2DE500, 0x01CF9BCF}
 	expect := time.Unix(1404949554, 0)
 	if !ft.Time().Equal(expect) {
-		t.Errorf("Date type: expect %v to equal %v", ft, expect)
+		t.Errorf("Date type: expect %v to equal %v", ft.Time(), expect)
 	}
 }
